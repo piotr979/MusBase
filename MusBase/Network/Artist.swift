ffa5b2f61f3ccb067996d4c1abb06ea0aae3a45c
Piotr Glaza <peterglaza@gmail.com>
2020-05-23 10:40:19 +0100

    Try full top

---
+++ MusBase/Network/Artist.swift
@@ -0,0 +1,9 @@
+//
+//  Artist.swift
+//  MusBase
+//
+//  Created by Start on 18/05/2020.
+//  Copyright © 2020 Start. All rights reserved.
+//
+
+import Foundation
---
+++ MusBase/Network/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  MusBase
+//
+//  Created by Start on 18/05/2020.
+//  Copyright © 2020 Start. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager: ObservableObject {
+    
+    @Published var artist: Artist
+}
